a779865925de1ffcb63f87e18454f73f5cc73977
dawd <maylarpp@gmail.com>
2026-09-23 00:44:06 -0400

    Add files via upload

--- clock.js
+++ clock.js
@@ -1,5 +1,14 @@
 const clocks = document.querySelectorAll('.local-time');
 
+let favicon = document.querySelector('link[rel="icon"]');
+if (!favicon) {
+  favicon = document.createElement('link');
+  favicon.rel = 'icon';
+  favicon.type = 'image/svg+xml';
+  favicon.href = 'assets/favicon.svg';
+  document.head.appendChild(favicon);
+}
+
 function updateClocks() {
   const now = new Date();
   const time = now.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
